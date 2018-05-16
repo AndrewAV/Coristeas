cef4c3d6c0c1cef0f5461b981c745979fb2972a7
Adrián Hernández Soto <hernandezad@estudiantec.cr>
2018-05-16 12:58:45 -0600

    Creación de Script camara
    camara

---
+++ Coristeas/Assets/Scripts/camara.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class camara : MonoBehaviour {
+    private float offsetX;
+    public float positionFinal;
+    public GameObject roca;
+    private float posicionInicial;
+    public bool retornar;
+    public float velocidad;
+    private Vector3 posicionInicialRoca;
+
+
+    // Use this for initialization
+    void Start() {
+        posicionInicial = this.gameObject.transform.position.x;
+        posicionInicialRoca = roca.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        offsetX = this.gameObject.transform.position.x - roca.transform.position.x;
+        if(offsetX <= 0 && this.gameObject.transform.position.x < positionFinal && !retornar)
+        {
+            this.gameObject.transform.position = new Vector3(roca.transform.position.x,this.gameObject.transform.position.y, this.gameObject.transform.position.z);
+        }
+        else if(retornar)
+        {
+            if(this.gameObject.transform.position.x > posicionInicial)
+            {
+                this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x - velocidad, this.gameObject.transform.position.y, this.gameObject.transform.position.z);
+                
+            }
+            else {
+                retornar = false;
+                roca.transform.position = posicionInicialRoca;
+            }
+                   
+        }
+    }
+}
