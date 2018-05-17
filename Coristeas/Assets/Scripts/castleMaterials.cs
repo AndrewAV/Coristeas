1c2872bc39eb4a7e519198c1db025b347a529fd3
Andrew Alvarado <andrew.jose.av@gmail.com>
2018-05-17 14:42:02 -0600

    Script de camara

---
+++ Coristeas/Assets/Scripts/castleMaterials.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class castleMaterials : MonoBehaviour
+{
+    //Aguante del material.
+    public int vida;
+    //Velocidad minima a la cual se hace daño.
+    private float velocidadMinima = 20f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.relativeVelocity.magnitude);
+        //Si es golpeada a mayor velocidad que la minima.
+        if (collision.relativeVelocity.magnitude > velocidadMinima)
+        {
+            quitarVida();
+        }
+    }
+
+    //Resta vida al material.
+    private void quitarVida()
+    {
+        vida--;
+        //Se destruye si se queda sin vida.
+        if (vida == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+
+
+}
+
---
+++ Coristeas/Assets/Scripts/rock.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rock : MonoBehaviour {
+
+    public Rigidbody2D rb;
+
+    public Rigidbody2D anchor;
+    //Tiempo luego del disparo para que desaparezca el resorte.
+    public float realeseTime= 0.15f;
+    //Si presiona la bola.
+    private bool isPressed = false;
+
+    private float maxDragDistance = 2f;
+
+    private void Update()
+    {
+        if (isPressed)
+        {
+            //Para que la bola pueda ser arrastrada.
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //Si sobre pasa el limite de arrastre.
+            if (Vector3.Distance(mousePos, anchor.position) > maxDragDistance)
+            {
+                rb.position = anchor.position + ( mousePos - anchor.position).normalized * maxDragDistance;
+            }
+            else
+            {  
+                rb.position = mousePos;
+            }
+           
+        }
+    }
+    //Si presiona la bola.
+    private void OnMouseDown()
+    {
+        isPressed = true;
+        //kinematic para que no sea arrastrada por el resorte mientras se esta presionando.
+        rb.isKinematic=true;
+    }
+
+    //Si se suelta la bola.
+    private void OnMouseUp()
+    {
+        isPressed = false;
+        rb.isKinematic = false;
+        StartCoroutine( Release());
+    }
+
+    //Rutina para soltar el resorte luego de un tiempo.
+    IEnumerator Release()
+    {
+        //Se espera un tiempo.
+        yield return new WaitForSeconds(realeseTime);
+        //Luego suelta el Spring para que la bola vuele libre.
+        GetComponent<SpringJoint2D>().enabled = false;
+        //Para que no se pueda manipular mas la bola.
+        //this.enabled = false;
+    }
+}
